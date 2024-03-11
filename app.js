63d80c034b72185e959f7e35084313e2f267cc2b
iqoqacola <iqoqacola@gmail.com>
2024-03-11 16:03:07 +0700

    add new features remove and list contact

--- app.js
+++ app.js
@@ -1,38 +1,70 @@
 const yargs = require("yargs");
-const { makeContact } = require("./contact");
+const { makeContact, listContact, removeContact } = require("./contact");
 
 yargs
     .command({
         command: 'add',
         describe: 'Add Contact',
         builder: {
+            // Change here if you want to add other personal data
             name: {
+                alias: 'na',
                 describe: 'Full Name',
                 demandOption: true,
                 type: 'string'
             },
             number: {
+                alias: 'nu',
                 describe: 'Phone Number',
                 demandOption: true,
                 type: 'string'
             },
-            email : {
+            email: {
+                alias: 'e',
                 describe: 'Email',
                 demandCommand: false,
                 type: 'string',
             }
         },
         handler(argv) {
+            // Change here too (Change contact variable)
             const contact = {
-                name : argv.name,
-                phoneNumber : argv.number,
+                name: argv.name,
+                phoneNumber: argv.number,
                 email: argv.email,
             }
-            
+
             makeContact(contact)
         },
 
     })
     .demandCommand()
-    .parse()
+
+yargs
+    .command({
+        command: 'list',
+        describe: 'See All Contact',
+        handler() {
+            listContact();
+        }
+    })
+
+yargs
+    .command({
+        command: 'remove',
+        describe: 'Remove Contact',
+        builder: {
+            name: {
+                alias: 'na',
+                describe: 'Full Name',
+                demandOption: true,
+                type: 'string'
+            },
+        },
+        handler(argv) {
+            removeContact(argv.name);
+        }
+    })
+
+yargs.parse()
 
--- contact.js
+++ contact.js
@@ -16,21 +16,20 @@ if (!existsSync(filePath)) {
 
 const makeContact = (...value) => {
     const contact = { ...value };
-    const fileBuffer = readFileSync(filePath, 'utf-8')
 
-
-    if (!validator.isMobilePhone(contact[0]['phoneNumber'], 'id-ID')) {
+    if (!validator.isMobilePhone(contact[0]['phoneNumber'], 'any')) {
         console.log(`${contact[0]['phoneNumber']} not phone number`);
         return false;
     }
 
-    if (contact[0]['email']){
-        if(!validator.isEmail(contact[0]['email'])){
+    if (contact[0]['email']) {
+        if (!validator.isEmail(contact[0]['email'])) {
             console.log(`${contact[0]['email']} not Email`)
             return false;
         }
     }
-    
+
+    const fileBuffer = readFileSync(filePath, 'utf-8')
     const contacts = JSON.parse(fileBuffer);
     contacts.push(contact[0]);
     writeFileSync(filePath, JSON.stringify(contacts))
@@ -38,4 +37,27 @@ const makeContact = (...value) => {
 
 }
 
-module.exports = { makeContact }
+const listContact = () => {
+    const fileBuffer = readFileSync(filePath, 'utf-8')
+    const contacts = JSON.parse(fileBuffer);
+    contacts.forEach((element, i) => {
+        console.log(`${i + 1}. ${element.name} - ${element.phoneNumber}`)
+    });
+}
+
+const removeContact = (name) => {
+
+    const fileBuffer = readFileSync(filePath, 'utf-8')
+    const contacts = JSON.parse(fileBuffer);
+
+    const newContacts = contacts.filter(n => n.name.toLowerCase() !== name.toLowerCase());
+
+    if(contacts.length === newContacts.length){
+        console.log(`${name} Not Found`)
+        return false;
+    }
+    writeFileSync(filePath, JSON.stringify(newContacts))
+    console.log(`Success : ${name} has been removed from contact`);
+}
+
+module.exports = { makeContact, listContact, removeContact }
